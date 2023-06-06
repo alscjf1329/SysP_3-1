7f134505c94dda1245439f50f545ab9724e80148
alscjf1329 <alscjf1329@gmail.com>
2023-06-06 15:17:56 +0900

    feat) add "-n" option

--- Sys_final/myhead.c
+++ Sys_final/myhead.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void printHead(const char* filename, int numLines) {
     FILE* file = fopen(filename, "r");
@@ -18,12 +20,20 @@ void printHead(const char* filename, int numLines) {
 
 int main(int argc, char** argv) {
     if (argc < 3) {
-        fprintf(stderr, "Usage: ./head <filename> <num_lines>\n");
+        fprintf(stderr, "Usage: ./head [-n <num_lines>] <filename>\n");
         return 1;
     }
 
-    const char* filename = argv[1];
-    int numLines = atoi(argv[2]);
+    int numLines = 10;  // 기본적으로 10줄을 출력
+    const char* filename = NULL;
+
+    if (strcmp(argv[1], "-n") == 0) {
+        numLines = atoi(argv[2]);
+        filename = argv[3];
+    }
+    else {
+        filename = argv[1];
+    }
 
     printHead(filename, numLines);
 
