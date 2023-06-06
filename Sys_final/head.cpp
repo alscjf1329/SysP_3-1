af4618f3bab49c8fac6a697abae0f49d96e7ce27
alscjf1329 <alscjf1329@gmail.com>
2023-06-06 14:21:36 +0900

    feat) head command

---
+++ Sys_final/head.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void printHead(const std::string& filename, int numLines) {
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        std::string line;
+        int count = 0;
+        while (std::getline(file, line) && count < numLines) {
+            std::cout << line << std::endl;
+            count++;
+        }
+        file.close();
+    }
+    else {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cerr << "Usage: ./head <filename> <num_lines>" << std::endl;
+        return 1;
+    }
+
+    std::string filename(argv[1]);
+    int numLines = std::stoi(argv[2]);
+
+    printHead(filename, numLines);
+
+    return 0;
+}
