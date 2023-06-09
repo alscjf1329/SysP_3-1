6252425d5a6460fd6ccb4ac0beab4026595305f7
alscjf1329 <alscjf1329@gmail.com>
2023-06-09 17:35:03 +0900

    refact) myhostname.c

--- Sys_final/myhostname.c
+++ Sys_final/myhostname.c
@@ -4,12 +4,12 @@
 
 int main(int argc, char* argv[])
 {
-	    char host_name[80];
-	        
-	        if(gethostname(host_name, sizeof(host_name)) != 0) {
-			        printf("gethostname() 실행 실패\n");
-				        exit(1);
-					    }
-		    printf("%s\n", host_name);
-		        return 0;
+	char host_name[80];
+
+	if (gethostname(host_name, sizeof(host_name)) != 0) {
+		printf("gethostname() 실행 실패\n");
+		exit(1);
+	}
+	printf("%s\n", host_name);
+	return 0;
 }
