2d5c1e6b27eea89ea08a474ccb22ba43b60d5f65
alscjf1329 <alscjf1329@gmail.com>
2023-06-09 17:57:48 +0900

    refact) myyes

--- Sys_final/myyes.c
+++ Sys_final/myyes.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    char *message = "y";
-    
-    if (argc > 1){;
+    // 인자가 들어올 시
+    if (argc > 1){
         while (1){
             for (int i=1; i<argc; i++){
                 printf("%s ", argv[i]);
@@ -11,9 +10,10 @@ int main(int argc, char *argv[]) {
             printf("\n");
         }
     }
+    // 인자가 없을 시
     else {
         while (1) {
-        printf("%s\n", message);
+            printf("y\n");
         }
     }
 
