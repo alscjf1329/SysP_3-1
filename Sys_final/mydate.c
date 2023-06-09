0e94a9238cc2df46c2c7d68ef112961d6d4a2bcd
alscjf1329 <alscjf1329@gmail.com>
2023-06-09 15:49:27 +0900

    gcc mycat.c

--- Sys_final/mydate.c
+++ Sys_final/mydate.c
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-#include <time.h>
-
-int main() {
-    time_t current_time;
-    struct tm* time_info;
-    char time_string[50];
-
-    // 현재 시간 정보를 가져옴
-    time(&current_time);
-    time_info = localtime(&current_time);
-
-    // 형식화된 시간 문자열로 변환
-    strftime(time_string, sizeof(time_string), "%c", time_info);
-
-    // 출력
-    printf("%s\n", time_string);
-
-    return 0;
-}
-=======
 #include <stdio.h>
 #include <time.h>
 
@@ -42,4 +20,3 @@ int main() {
 
     return 0;
 }
->>>>>>> 34e2fa90470725649cb1da6c75524f1676c5e646
